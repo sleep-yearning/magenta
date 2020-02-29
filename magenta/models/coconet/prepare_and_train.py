482d7d846e1597d0cdf2ca9d3e08118e26fe4b85
sleep-yearning <emailfurunity@web.de>
2020-02-29 12:47:51 +0100

    made file callable with parameters

--- magenta/models/coconet/prepare_and_train.py
+++ magenta/models/coconet/prepare_and_train.py
@@ -1,17 +1,15 @@
-path = '/home/noah/Document/Studiumkram/ACM/Projekt/coconet/abba/'
-epochs = 0
-grouped = True
-modelpath = '/home/noah/Document/Studiumkram/ACM/Projekt/coconet/models/abba/'
-
 from magenta.models.coconet import analyze_instruments, midi_folder_transversion, coconet_train
 import tensorflow as tf
 
-interpret_instruments = analyze_instruments.find_frequent_programs(path, grouped)
-converted_data = midi_folder_transversion.convert_folder(path)
-min_pitch = min(converted_data)
-max_pitch = max(converted_data)
-
-train_args = [
+def main(path,epochs,grouped,modelpath):
+    
+    interpret_instruments = analyze_instruments.find_frequent_programs(path, grouped)
+    converted_data = midi_folder_transversion.convert_folder(path)
+    
+    min_pitch = min(converted_data)
+    max_pitch = max(converted_data)
+    
+    train_args = [
     'logdir=' + path + 'log/',
     'log_process=True',
     'data_dir=' + path,
@@ -40,13 +38,16 @@ train_args = [
     'dilate_time_only=False',
     'repeat_last_dilation_level=False',
     'num_pointwise_splits=2',
-    'interleave_split_every_n_layers=2'
-]
+    'interleave_split_every_n_layers=2']
 
-
-def main():
     tf.app.run(main=coconet_train.main(), argv=train_args)
 
 
 if __name__ == '__main__':
-    main()
+    import sys
+    args = sys.argv[1:]
+    if len(args) < 4:
+        raise ValueError('not enough arguments, user must specify in order: '+
+                         'Midi Folder path, epochs to train, are instruments grouped, model save path')
+    else:
+        main(args)
