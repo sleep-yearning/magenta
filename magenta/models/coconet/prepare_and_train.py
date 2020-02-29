c03d3356f20fe35a92b461332a4d1357093ab09b
sleep-yearning <emailfurunity@web.de>
2020-02-29 12:18:45 +0100

    fixed imports and naming convention

--- magenta/models/coconet/prepare_and_train.py
+++ magenta/models/coconet/prepare_and_train.py
@@ -3,28 +3,26 @@ epochs = 0
 grouped = True
 modelpath = '/home/noah/Document/Studiumkram/ACM/Projekt/coconet/models/abba/'
 
-import analyzeInstruments
-import midiFolderTransversion
-from magenta.models.coconet import coconet_train
+from magenta.models.coconet import analyze_instruments, midi_folder_transversion, coconet_train
 import tensorflow as tf
 
-interpretInstruments = analyzeInstruments.findFrequentPrograms(path, grouped)
-convertedData = midiFolderTransversion.convertFolder(path)
-minPitch = min(convertedData)
-maxPitch = max(convertedData)
+interpret_instruments = analyze_instruments.find_frequent_programs(path, grouped)
+converted_data = midi_folder_transversion.convert_folder(path)
+min_pitch = min(converted_data)
+max_pitch = max(converted_data)
 
 train_args = [
     'logdir=' + path + 'log/',
     'log_process=True',
     'data_dir=' + path,
     # Save instruments with model config
-    'program1=' + interpretInstruments[0],
-    'program2=' + interpretInstruments[1],
-    'program3=' + interpretInstruments[2],
-    'program4=' + interpretInstruments[3],
-    'rhythmProgramChannel10=' + interpretInstruments[4],
-    'min_pitch='+minPitch,
-    'max_pitch='+maxPitch,
+    'program1=' + interpret_instruments[0],
+    'program2=' + interpret_instruments[1],
+    'program3=' + interpret_instruments[2],
+    'program4=' + interpret_instruments[3],
+    'rhythmProgramChannel10=' + interpret_instruments[4],
+    'min_pitch='+min_pitch,
+    'max_pitch='+max_pitch,
     # Data preprocessing.
     'dataset=TestData',
     'crop_piece_len=32',
