3a578c9efab3145fc5bd9cf0a2b74b2bdfbc1c4e
Tessa <tessa.lottermann@gmx.de>
2020-03-16 12:08:57 +0100

    PEP8 reformatting, fixing of sampling bug

--- magenta/models/coconet/GUI.py
+++ magenta/models/coconet/GUI.py
@@ -30,7 +30,6 @@ own_checkpoint_path = StringVar()
 sample_midi_path = StringVar()
 title_new_train_model = StringVar()
 nepochs = StringVar()
-model_checkpoint_folder_path = StringVar()
 sampled_midi = StringVar()
 
 is_grouped_pre = BooleanVar()
@@ -81,7 +80,6 @@ size_batch_strategy.set(3)
 piece_length.set(32)
 temperature.set(0.99)
 
-
 # This map contains the pre-trained models and the paths to their data as a basis for the model selection
 
 base_path = str(pathlib.Path(__file__).parent.absolute())
@@ -94,21 +92,24 @@ model_map = {
     "Zelda": os.path.join(base_path, "trained_models/")
 }
 
+
 # Definitions go here
 
-#To check if train folder contains TrainData.npz
+# To check if train folder contains TrainData.npz
 def check_that_folder_contains_file(folder_path, filename):
     path = os.path.join(folder_path, filename)
     return os.path.isfile(path)
 
-#Set folder for preprocessing
+
+# Set folder for preprocessing
 def open_converting_folder():
     folder_path = (filedialog.askdirectory(parent=root, title='Choose a folder containing midi files') + '/')
     convert_folder_path.set(folder_path)
     result_folder_path_npz.set(folder_path)
     print(convert_folder_path)
 
-#Set folder for training
+
+# Set folder for training
 def open_train_folder():
     folder_path = filedialog.askdirectory(parent=root, title='Choose a folder containing npz file')
     train_folder_path.set(os.path.join(folder_path, ''))
@@ -116,52 +117,61 @@ def open_train_folder():
         result_folder_path_training.set(folder_path)
     print(folder_path)
 
-#Set folder for sampling
+
+# Set folder for sampling
 def open_result_folder_sampling():
     folder_path = filedialog.askdirectory(parent=root, title='Choose a folder to place the results in')
     sampling_folder_path.set(folder_path)
     print(folder_path)
 
-#Set folder with own pretrained model
+
+# Set folder with own pretrained model
 def open_own_checkpoint():
     folder_path = filedialog.askdirectory(parent=root, title='Choose a folder with your own Checkpoint')
     own_checkpoint_path.set(folder_path)
     print(folder_path)
 
-#Choose midi to sample from
+
+# Choose midi to sample from
 def open_sample_midi():
     folder_path = filedialog.askopenfilename(parent=root, filetypes=[("Midi Files", ".midi .mid")],
                                              title='Choose the midi file to sample from')
     sample_midi_path.set(folder_path)
     print(folder_path)
 
-#Select a generated midi to play
+
+# Select a generated midi to play
 def select_sampled_midi():
     folder_path = filedialog.askopenfilename(parent=root, filetypes=[("Midi Files", ".midi .mid")],
                                              title='Choose the midi file that you want to play')
     sampled_midi.set(folder_path)
     # print(folder_path)
 
-#start Preprocessing
+
+# start Preprocessing
 def start_preprocessing():
     prepare(convert_folder_path.get(), is_grouped_pre.get())
     print("Preprocessing done")
 
-#Use Preprocessing with multithreading to make the GUI still usable
+
+# Use Preprocessing with multithreading to make the GUI still usable
 def convert_in_background():
     _thread.start_new_thread(start_preprocessing, ())
 
-#Stop Button
+
+# Stop Button
 def stop_it():
     root.destroy()
 
-#Add the Pretrained Model to the sample page
+
+# Add the Pretrained Model to the sample page
 def add_model_to_menu():
     val = title_new_train_model.get()
     which_model['menu'].add_command(label=val, command=tk._setit(choosemodel, val))
     model_map[val] = train_folder_path.get()
 
-#Start training by checking the right type of hyperparameters first.
+
+# Start training by checking the right type of hyperparameters first.
 def start_training():
     string_nepochs = nepochs.get()
     try:
@@ -243,13 +253,15 @@ def start_training():
           num_filters=int_filters, num_layers=int_layers, num_dilation_blocks=int_db,
           num_pointwise_splits=int_pointwise, interleave_split_every_n_layers=int_interleave)
 
-#combine adding model to sample page and start training for Button command
+
+# combine adding model to sample page and start training for Button command
 def really_start_training():
     add_model_to_menu()
     start_training()
     print("Training done")
 
-#Run Preprocessing and training in one step
+
+# Run Preprocessing and training in one step
 def preptrain():
     string_nepochs = nepochs.get()
     try:
@@ -260,13 +272,15 @@ def preptrain():
 
     combo(train_folder_path.get(), int_nepochs, is_grouped_train.get(), title_new_train_model.get())
 
+
 # def play():
 #     model_name = choosemodel.get()
 #     model_folder_path = model_map[model_name]
 #     model_checkpoint_folder_path = os.path.join(model_folder_path, model_name + '_checkpoint')
 #     print(model_checkpoint_folder_path)
 
-#start sampling by checking the right input types first, afterwards checking if using a pretrained model or an own model
+# start sampling by checking the right input types first,
+# afterwards checking if using a pretrained model or an own model
 def start_sampling():
     string_batches_strategy = size_batch_strategy.get()
     try:
@@ -291,13 +305,12 @@ def start_sampling():
         messagebox.showerror("Error", "Temperature size must be a number between 0 and 1!")
         return
 
-    model_name = choosemodel.get()
-    model_folder_path = model_map[model_name]
-    model_checkpoint_folder_path = os.path.join(model_folder_path, model_name + '_checkpoint')
-
     if own_checkpoint_path != "":
         checkpoint = own_checkpoint_path.get()
     else:
+        model_name = choosemodel.get()
+        model_folder_path = model_map[model_name]
+        model_checkpoint_folder_path = os.path.join(model_folder_path, model_name + '_checkpoint')
         checkpoint = model_checkpoint_folder_path
 
     sample(checkpoint=checkpoint, tfsample=tfsample.get(), strategy=choosestrategy.get(),
@@ -306,7 +319,8 @@ def start_sampling():
 
     messagebox.showinfo("Info", "Sampling done!")
 
-#play a midi file
+
+# play a midi file
 def play_midi():
     pygame.midi.init()
     pygame.mixer.init()
@@ -369,12 +383,15 @@ btn_start_converting.pack()
 btn_stop_converting = ttk.Button(f1, text="Stop preprocessing", command=stop_it)
 btn_stop_converting.pack()
 
+Separator(f1, orient=HORIZONTAL).pack(fill='x', pady=10)
+
 lbl_do_preptrain = Label(f1,
-                    text="If you want to preprocess and train your data in one step you can do it in the >>Training<< Tab")
+                         text="If you want to preprocess and train your data in "
+                              "one step you can do it in the >>Training<< Tab")
 lbl_do_preptrain.pack()
 
-#progress = ttk.Progressbar(f1, orient=HORIZONTAL, length=200)
-#progress.pack()
+# progress = ttk.Progressbar(f1, orient=HORIZONTAL, length=200)
+# progress.pack()
 
 #########
 #
@@ -518,13 +535,13 @@ choosemodel.set(OptionList[0])
 
 which_model = OptionMenu(f3, choosemodel, *OptionList)
 which_model.config()
-which_model.pack() 
+which_model.pack()
 
 lbl_frame_own_checkpoint_folder = ttk.LabelFrame(f3, text="OR select a folder containint your own checkpoint")
 lbl_frame_own_checkpoint_folder.pack()
 
 btn_own_checkpoint_folder = ttk.Button(lbl_frame_own_checkpoint_folder, text="Select Folder",
-                                      command=open_own_checkpoint)
+                                       command=open_own_checkpoint)
 btn_own_checkpoint_folder.pack()
 
 lbl_own_checkpoint_folder = Label(master=f3, textvariable=own_checkpoint_path)
